2a9829f78df1a78a6e054b91a89c2136d06000aa
Jay Goss <jayspgoss@gmail.com>
2018-12-09 22:43:27 -0500

    updates

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -1,10 +1,18 @@
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "view.html"));
+var path = require("path");
+
+module.exports = function(app) {
+//  A GET Route to /survey which should display the survey page.
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
   });
 
-//  A GET Route to /survey which should display the survey page.
-
-
-
 // A default, catch-all route that leads to home.html which displays the home page.
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+  });
+
+// default to home page if any other url chosen
+app.use( function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+  });
+};
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,10 +1,22 @@
 // require the basic npm packages we've used in class: express and path.
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// require my data page 
+// require("./app/data/friends.js")(app);
+
+//require my routing pages
+// require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
