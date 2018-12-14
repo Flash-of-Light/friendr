bae6817749697049010a6a2c85722d2d44acddcc
Jay Goss <jayspgoss@gmail.com>
2018-12-14 17:16:04 -0500

    matching

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -11,13 +11,38 @@ app.get("/api/friends", function(req, res) {
 // A POST routes /api/friends. This will be used to handle incoming survey results.
 // This route will also be used to handle the compatibility logic.
 app.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
+    var match = {
+      name: "",
+      photo: "",
+      friendDifference: 50
+    };
+
     var newFriend = req.body;
-    console.log(newFriend);
+    var userScores = newFriend.scores;
+    var totalDifference = 0;
+
+    for (var i = 0; i < friendArray.length; i++) {
+
+      console.log(friendArray[i].name);
+      totalDifference = 0;
+
+      // loop through the scores of each friend
+      for (var j = 0; j < friendArray[i].scores[j]; j++) {
+
+        // calculate the difference between the scores (totalDifference)
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendArray[i].scores[j]));
+
+        // If the sum of differences is less than the differences of the current "best match"
+        if (totalDifference < match.friendDifference) {
+
+          // Reset the match to be the new friend.
+          match.name = friendArray[i].name;
+          match.photo = friendArray[i].photo;
+          match.friendDifference = totalDifference;
+        }
+      }
+    }
     friendArray.push(newFriend);
-  
-    // We then display the JSON to the users
-    res.json(newFriend);
-  });
+    res.json(match);
+});
 };
